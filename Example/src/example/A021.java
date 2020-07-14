f8135ea9c7a475ee0f861747f2f003a59e4b6dfd
1 <1@1-PC>
2020-07-14 11:48:49 +0900

    채팅문만들기

--- Example/src/example/A021.java
+++ Example/src/example/A021.java
@@ -1,12 +1,29 @@
 package example;
 
+import java.util.Scanner;
+
 public class A021 {
 
 	public static void main(String[] args) {
 		//do while문을 이용한 채팅 창 만들기!
 		// 종료를 입력하면 채팅 끝~
+		Scanner scan = new Scanner(System.in);
+		boolean end = true;
+		String chat = "";
+		
+		System.out.println("※내용을 입력하세요※");
+		do {
+			System.out.print("▷ ");
+			chat = scan.nextLine();
+			System.out.println("-"+chat);
+			if (chat.equals("종료")) {
+				end = false;
+				System.out.println("\n채팅을 끝냅니다.");
+			}
+		}while(end);
 		
 
+		scan.close();
 	}
 
 }
