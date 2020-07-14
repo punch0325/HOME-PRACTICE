d8b4decab10c6658ba54dde8c1f3df6082f84965
1 <1@1-PC>
2020-07-14 14:16:00 +0900

    집에서 연습하기!

--- Example/src/example/A022.java
+++ Example/src/example/A022.java
@@ -6,6 +6,20 @@ public class A022 {
 		//1-2+3-4+5-6+7.....을 계산해서 100 이상이 될 때,
 		//계산식의 마지막 숫자를 출력!
 		
+		int num1 = 0;
+		int num2 = 0;
+		int sum = 0;
+		int m = 0;
+		boolean end = true;
+		
+		do {
+			
+			sum+=num1;
+			if(sum >= 100) {
+				end = false;
+				System.out.println(num1);
+			}
+		}while(end);
 		
 		
 		
---
+++ Example/src/example/A032.java
@@ -0,0 +1,13 @@
+package example;
+
+public class A032 {
+
+	public static void main(String[] args) {
+	     //학생 점수 관리 프로그램 만들기 (트렐로 7/14)
+		
+		
+		
+
+	}
+
+}
