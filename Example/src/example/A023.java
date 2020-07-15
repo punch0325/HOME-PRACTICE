e0575b7478545488767f8fa73ff75c9656c44fc8
1 <1@1-PC>
2020-07-15 13:38:55 +0900

    연습!

--- Example/src/example/A023.java
+++ Example/src/example/A023.java
@@ -1,12 +1,44 @@
 package example;
 
+import java.util.Scanner;
+
 public class A023 {
 
 	public static void main(String[] args) {
 		//정수를 무한 반복하여 입력 후 0이 되면 종료!
 		//입력한 숫자의 총 개수와 합계 평균도 출력!
 		
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요. 0을 누르면 종료됩니다.");
+		int num = 0;
+		int count = 0;
+		int sum = 0;
+		int average = 0;
+		boolean end = true;
+		
+		do {
+			num = scan.nextInt();
+			
+			if (num > 0||num < 0) {
+				sum+=num;
+				count++;
+			}else if (num == 0) {
+				end = false;
+				System.out.println("프로그램 종료");
+			}
+		}while (end);
+		average = sum/count;
+		System.out.println();
+		System.out.println("총 입력 개수는 "+count+"입니다.");
+		System.out.println("숫자 총 합계 : "+sum);
+		System.out.println("숫자 총 평균 : "+average);
+	
+	
+		scan.close();
+	
+	
+	
+	
 	}
 
 }
--- Example/src/example/A024.java
+++ Example/src/example/A024.java
@@ -1,11 +1,43 @@
 package example;
 
+import java.util.Scanner;
+
 public class A024 {
 
 	public static void main(String[] args) {
 		//UP & DOWN 게임 만들기!
 		
-
+		System.out.println("★UP & DOWN★");
+		System.out.println();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num1 = 0;
+		int num2 = 0;
+		int count = 0;
+		boolean end = true;
+		num1 = (int)(Math.random()*100)+1;
+		
+		while (end) {	
+			System.out.print("1-100 까지의 숫자 중 하나를 입력하세요  : ");
+			num2 = scan.nextInt();
+			
+			if (num1 > num2) {
+				System.out.println("※UP!");
+				count++;
+			}else if (num1 < num2) {
+				System.out.println("※DOWN!");
+				count++;
+			}else if (num1 == num2) {
+				end = false;
+				count++;
+				System.out.println("성공입니다♥");
+			}
+		} 
+		System.out.println("총 시도 횟수는 "+count+"번!");
+	
+		scan.close();
+	
 	}
 
 }
